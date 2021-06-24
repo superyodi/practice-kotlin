be3d475682990103c8d92014b7aa72c6a199bf76
superyodi <songagreen@gmail.com>
2021-06-24 13:24:15 +0900

    leetcode의 interview 문제 dir 추가

--- src/leetcode_interview/ReverseInteger.kt
+++ src/leetcode_interview/ReverseInteger.kt
@@ -1,4 +1,28 @@
 package leetcode_interview
 
+
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/880/
 class ReverseInteger {
+
+    fun reverse(x: Int): Int {
+        var x = x
+        var rev = 0
+        while (x != 0) {
+            val pop = x % 10
+            x /= 10
+            if (rev > Int.MAX_VALUE / 10 || rev == Int.MAX_VALUE / 10 && pop > 7) return 0
+            if (rev < Int.MIN_VALUE / 10 || rev == Int.MIN_VALUE / 10 && pop < -8) return 0
+            rev = rev * 10 + pop
+        }
+        return rev
+
+    }
 }
+
+fun main() {
+
+    val reverseInteger = ReverseInteger()
+    print(reverseInteger.reverse(123))
+}
+
+
--- src/leetcode_interview/ReverseString.kt
+++ src/leetcode_interview/ReverseString.kt
@@ -1,4 +1,23 @@
 package leetcode_interview
 
+
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/879/
+
 class ReverseString {
+    fun reverseString(s: CharArray): Unit {
+
+        var f = 0
+        var t = s.size-1
+        var tmp: Char = 'a'
+
+        while(f < t) {
+
+            tmp = s[f]
+            s[f] = s[t]
+            s[t] = tmp
+
+            f += 1
+            t -= 1
+        }
+    }
 }
